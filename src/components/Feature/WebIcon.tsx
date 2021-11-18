6c6dc2a18f983fddb3c2230fbbbf00afd60114fc
VadimGouskov <vadimgouskov@hotmail.com>
2021-11-18 14:27:29 +0100

    styled WebIcon

--- src/components/Feature/WebIcon.tsx
+++ src/components/Feature/WebIcon.tsx
@@ -4,26 +4,34 @@ type Props = {
     imgSrc: string;
     offsetX?: string;
     offsetY?: string;
+    logoWidth?: string;
+    rounded?: string;
 };
 
-const WebIcon: React.FC<Props> = ({imgSrc, offsetX = "0px", offsetY = "0px"}) => {
+const WebIcon: React.FC<Props> = ({
+    imgSrc,
+    offsetX = "0px",
+    offsetY = "0px",
+    logoWidth = "85%",
+    rounded = "100%",
+}) => {
     return (
         <Box
             width="33%"
-            rounded="100%"
+            rounded={rounded}
             position="absolute"
             left={offsetX}
             top={offsetY}
             transform="translate(-50%, -50%)"
             backgroundColor="white"
-            border="1px solid red"
+            boxShadow="rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px"
             allignItems="center"
             // We want the height to be equal to the width  https://mademyday.de/height-equals-width-with-pure-css/
             _before={{content: "''", float: "left", paddingTop: "100%"}}
         >
             <Img
                 aspectRatio={1 / 1}
-                htmlWidth="90%"
+                htmlWidth={logoWidth}
                 //height={{md: "100px"}}
                 objectFit="contain"
                 src={imgSrc}
