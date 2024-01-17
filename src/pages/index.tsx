ec2f6192b44b1ae825d78aa165ce67549efea802
Vadim Gouskov <vadimgouskov@hotmail.com>
2024-01-17 08:24:09 +0100

    fix: removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,20 @@
 import Hero from "../components/Hero";
 import React from "react";
-import Feature from "../components/Feature";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { GetStaticPropsResult } from "next";
-import { useTranslation } from "next-i18next";
-import BlobFeature from "../components/Feature/BlobFeature";
-import { Box, Img } from "@chakra-ui/react";
-import WebIcon from "../components/Feature/WebIcon";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {GetStaticPropsResult} from "next";
+import {useTranslation} from "next-i18next";
+import {Box, Img} from "@chakra-ui/react";
 import WebDevelopment from "../components/Home/WebDevelopment";
 import CreativeCoding from "../components/Home/CreativeCoding";
 import Otli from "../components/Home/Otli";
 
 const IndexPage: React.FC = () => {
-    const { t } = useTranslation("index");
+    const {t} = useTranslation("index");
 
     return (
         <>
             <Hero></Hero>
-            <Box overflowX={{ base: "hidden", lg: "visible" }}>
+            <Box overflowX={{base: "hidden", lg: "visible"}}>
                 <WebDevelopment></WebDevelopment>
                 <CreativeCoding></CreativeCoding>
                 <Otli></Otli>
@@ -28,7 +25,7 @@ const IndexPage: React.FC = () => {
 
 export default IndexPage;
 
-export async function getStaticProps({ locale }): Promise<GetStaticPropsResult<any>> {
+export async function getStaticProps({locale}): Promise<GetStaticPropsResult<any>> {
     return {
         props: {
             ...(await serverSideTranslations(locale, ["index"])),
