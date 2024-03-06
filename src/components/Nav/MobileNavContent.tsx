814b6a1f342dd74fe41371769a414ade2737d4ce
Vadim Gouskov <vadim@acostudio.be>
2024-03-06 08:27:58 +0100

    fix: removed framer motion dependancies

--- src/components/Nav/MobileNavContent.tsx
+++ src/components/Nav/MobileNavContent.tsx
@@ -1,5 +1,4 @@
-import {CloseButton, Flex, useColorModeValue} from "@chakra-ui/react";
-import {AnimatePresence, motion} from "framer-motion";
+import {CloseButton, Flex} from "@chakra-ui/react";
 import * as React from "react";
 
 interface Props {
@@ -9,34 +8,26 @@ interface Props {
 
 export const MobileNavContent: React.FC<Props> = (props) => {
     const {isOpen, onClose, children} = props;
+
+    if (!isOpen) return <></>;
+
     return (
-        <AnimatePresence>
-            {isOpen && (
-                <motion.div
-                    transition={{duration: 0.1}}
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    exit={{opacity: 0}}
-                >
-                    <Flex
-                        direction="column"
-                        w="100%"
-                        bg="black"
-                        color="white"
-                        h="100vh"
-                        overflow="auto"
-                        pos="absolute"
-                        top={0}
-                        left={0}
-                        zIndex={20}
-                        px={4}
-                        py={4}
-                    >
-                        {children}
-                        <CloseButton pos="absolute" top={4} right={4} onClick={onClose} />
-                    </Flex>
-                </motion.div>
-            )}
-        </AnimatePresence>
+        <Flex
+            direction="column"
+            w="100%"
+            bg="black"
+            color="white"
+            h="100vh"
+            overflow="auto"
+            pos="absolute"
+            top={0}
+            left={0}
+            zIndex={20}
+            px={4}
+            py={4}
+        >
+            {children}
+            <CloseButton pos="absolute" top={4} right={4} onClick={onClose} />
+        </Flex>
     );
 };
