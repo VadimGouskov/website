c385e79a4cf892b9842685fe466ea53294111797
Vadim Gouskov <vadim@acostudio.be>
2025-02-14 14:54:28 +0100

    feat: hover effect for small tiles

--- src/app/home/components/ImageTile.tsx
+++ src/app/home/components/ImageTile.tsx
@@ -10,8 +10,14 @@ type ImageTileProps = {
 
 export const ImageTile: React.FC<ImageTileProps> = ({ image }) => {
   return (
-    <div className="h-full relative rounded-sm overflow-hidden border">
-      <Image className="object-cover" fill src={image.src} alt={image.alt} />
+    <div className="relative h-full hover:[&>.hover-effect]:scale-110">
+      <div
+        className="hover-effect bg-amber-100 absolute top-0 left-0 h-full w-full -z-10 transition-transform 
+"
+      ></div>
+      <div className="relative h-full rounded-sm overflow-hidden border">
+        <Image className="object-cover" fill src={image.src} alt={image.alt} />
+      </div>
     </div>
   );
 };
