8b5393d3131d10d15bef44a57e5850fdd844172d
Vadim Gouskov <vadim@acostudio.be>
2025-02-20 21:19:39 +0100

    fixed current url import

--- loader.ts
+++ loader.ts
@@ -1,25 +1,18 @@
 "use client";
 
-import { getCurrentUrl } from '@/lib/getCurrentUrl';
-import { useEffect, useState } from 'react';
+import { getCurrentUrl } from "@/lib/getCurrentUrl";
+import { useEffect, useState } from "react";
 
-const customLoader = ({
-  src,
-  width,
-}: {
-  src: string
-  width: number
-}) => {
+const customLoader = ({ src, width }: { src: string; width: number }) => {
   const [currentUrl, setCurrentUrl] = useState(`${src}`);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-     return setCurrentUrl(`${getCurrentUrl()}${src}`);
- 
+    if (typeof window !== "undefined") {
+      return setCurrentUrl(`${getCurrentUrl()}${src}`);
     }
   }, [src]);
 
   return currentUrl;
-}
+};
 
 export default customLoader;
