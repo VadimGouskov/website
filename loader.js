b8c2bd348f5d010f87a7b894305aa8d4f6bd856f
Vadim Gouskov <vadim@acostudio.be>
2025-02-23 21:40:31 +0100

    fix image optimization and ad avif as image  type

--- loader.js
+++
@@ -1,7 +0,0 @@
-import { getCurrentUrl } from "@/lib/getCurrentUrl";
-
-const customLoader = ({ src, width, quality }) => {
-  return `${getCurrentUrl()}${src}?w=${width}q=${quality || 75}`;
-};
-
-export default customLoader;
--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+const staticExport = process.env.STATIC_EXPORT;
+
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  output: staticExport ? "export" : undefined,
   images: {
-    loader: "custom",
-    loaderFile: "./loader.js",
+    formats: ["image/avif", "image/webp"],
+    // unoptimized: staticExport ? true : false,
   },
 };
 
